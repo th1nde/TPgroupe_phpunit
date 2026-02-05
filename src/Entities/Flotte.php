56662375284fca28cc99fd013c3e3fdc20900a8e
th1nde <arnaud@monel.fr>
2026-02-05 16:01:15 +0100

    update class

--- src/Entities/Flotte.php
+++ src/Entities/Flotte.php
@@ -10,6 +10,42 @@ class Flotte {
 	private $etat;
 
 
+	public function getNom(){
+
+		return $this->nom;
+
+	}
+
+	public function getEnergie(){
+
+		return $this->energie;
+
+	}
+
+	public function getEtat() {
+
+		return $this->etat;
+	}
+
+	public function setNom($nom) {
+
+		$this->nom = $nom;
+	}
+
+	public function setEnergie($energie) {
+
+		$this->energie = $energie;
+
+	}
+
+	public function setEtat($etat) {
+
+		$this->etat = $etat;
+
+	}
+
+
+
 }
 
 
